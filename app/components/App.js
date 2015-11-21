1c43adbd728ddbac55a702fc65d119a398240dfe
christo <chrisgiggles@live.se>
2015-11-21 12:26:04 +0100

    Added more components

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <Navbar history={this.props.history} />
                 {this.props.children}
+                <Footer />
             </div>
         );
     }
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route } from 'react-router';
 import App from './components/App.js';
 import Home from './components/Home.js';
+import AddCharacter from './components/AddCharacter.js';
 
 export default (
     <Route component={ App }>
         <Route path="/" component={ Home } />
+        <Route path='/add' component={ AddCharacter } />
     </Route>
 )
